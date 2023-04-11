ab2b6422be79956c94321ee0995677cc174b33bd
kendalldrake <112967655+kendalldrake@users.noreply.github.com>
2023-04-11 13:58:38 -0400

    added elevator placeholder image

--- main.js
+++ main.js
@@ -1,4 +1,34 @@
 // You can put your individual, DOM-specific logic here.
-window.stateCallback = () => {
+// window.stateCallback = () => {
+// 	console.log('Something changed!')
+// }
+
+
+//making buttons clickable//
+heatmap=document.querySelector("#heatmap")
+document.querySelector("#B1").addEventListener("click",function(){
 	console.log('Something changed!')
-}
+	if(heatmap.style.display=="none"){
+		heatmap.style.display="block"
+	}
+	else{heatmap.style.display="none"}
+})
+
+demoJson={
+	week1: {
+	  monday:{
+		"12:00": 10,
+		"1:00": 30
+	  },
+	  tuesday:12,
+	  wednesday:30,
+  },
+	week2: {
+		monday:{
+		  "12:00": 10,
+		  "1:00": 30
+		},
+		tuesday:12,
+		wednesday:30,
+	}
+  }
